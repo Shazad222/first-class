66fe3083baa44c11a76c30d17cdb0adbaa20b3f6
APPLE <apple@APPLEs-MacBook.local>
2023-10-09 15:39:21 +0500

    Added code for first day feature

--- lib/icecream/icecream_view.dart
+++ lib/icecream/icecream_view.dart
@@ -22,13 +22,15 @@ class _IcecreamViewState extends State<IcecreamView> {
     loadIceream();
   }
 
-  Future<Map<String, dynamic>> loadIceream() async {
+  Future<List<Icecream>> loadIceream() async {
     final rawIceream = await rootBundle.loadString("assets/icecream.json");
-    final decodeIcream = await jsonDecode(rawIceream);
-    // final icecream = iceCreamFromJson(rawIceream);
+    // final decodeIcream = await jsonDecode(rawIceream);
+    final icecream = iceCreamFromJson(rawIceream);
     await Future.delayed(const Duration(seconds: 2));
-    return decodeIcream;
+    return icecream.icecreams;
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -55,33 +57,16 @@ class _IcecreamViewState extends State<IcecreamView> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              FutureBuilder<Map<String, dynamic>>(
-                future: loadIceream(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    // Check if the decoded JSON data is not null
-                    if (snapshot.data != null) {
-                      // Try to access the first ice cream flavor
-                      final firstFlavor =
-                          snapshot.data!["icecreams"][0]["flavor"];
+            FutureBuilder(
+    future: loadIceream(),
+    builder: (context, snapshot) {
+    if (snapshot.connectionState == ConnectionState.done) {
+    final icecreams = snapshot.data;
+    return Text(icecreams!.first.flavor);
+    } else
+    return CircularProgressIndicator();
 
-                      // If the first flavor is not null, show it
-                      if (firstFlavor != null) {
-                        return Text(firstFlavor);
-                      } else {
-                        return Text(
-                            "An error occurred while loading the ice cream data.");
-                      }
-                    } else {
-                      return Text(
-                          "An error occurred while loading the ice cream data.");
-                    }
-                  } else {
-                    return CircularProgressIndicator();
-                  }
-                },
-              )
-            ],
+    ],
           ),
         ))
       ],
